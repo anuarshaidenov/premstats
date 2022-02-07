fcead6cb29c888a0c32da5623a72ee657eb9a73f
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-02-08 00:25:52 +0600

    Add filtered search

--- src/components/teams-section/TeamsSection.js
+++ src/components/teams-section/TeamsSection.js
@@ -1,23 +1,43 @@
 import './teams-section.scss';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchTeamsFromAPI } from '../../redux/teams/thunk';
 import Team from '../team/Team';
 
 const TeamsSection = () => {
+  const [searchField, setSearchField] = useState('');
   const teams = useSelector((state) => state.teamsReducer.teams);
   const isLoading = useSelector((state) => state.teamsReducer.isLoading);
   const dispatch = useDispatch();
+
   useEffect(
-    () => !teams.length && dispatch(fetchTeamsFromAPI()),
+    () => !teams.length && dispatch(fetchTeamsFromAPI(2021)),
     [dispatch, teams.length]
   );
+
+  const handleSearchFieldChange = (e) => setSearchField(e.target.value);
+
+  const filteredTeams = teams.filter((team) =>
+    team.team.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
   return (
     <section className="teams-section container container--small">
+      <form className="teams-section__form">
+        <input
+          type="text"
+          placeholder="Teams..."
+          className="teams-section__input-search"
+          value={searchField}
+          onChange={handleSearchFieldChange}
+        />
+      </form>
       <ul className="teams-section__teams">
         {isLoading && `loading...`}
-        {teams.length
-          ? teams.map((team) => <Team key={team.team.id} team={team.team} />)
+        {filteredTeams.length
+          ? filteredTeams.map((team) => (
+              <Team key={team.team.id} team={team.team} />
+            ))
           : null}
       </ul>
     </section>
